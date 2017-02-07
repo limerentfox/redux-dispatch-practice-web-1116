89d7b41f9409a2a7841feb4082156d46af7b84f1
limerentfox <christi@limerentfox.com>
2017-02-07 11:47:35 -0500

    Done.

--- src/reducers/managePets.js
+++ src/reducers/managePets.js
@@ -1,12 +1,29 @@
 export let state;
 
 
-export function managePets(){
+export function managePets(state ={pets: []}, action ){
+  switch (action.type) {
+    case 'ADD_PET':
+    return {...state, pets:[...state.pets, action.payload]}
+    case 'REMOVE_PET':
+    let newPets = state.pets.filter((pet) => {
+      return pet.id !== action.payload
+    })
+
+    return {...state, pets: newPets}
+    default:
+      return state
+  }
 }
 
-export function dispatch(){
+export function dispatch( action ){
+  state = managePets(state, action)
+  render()
 }
 
 export function render(){
+  const petArray = state.pets
+  let container = document.getElementById('container')
+  container.innerHtml = `<ul>${petArray.map( pet =>  `<li>${pet.name}</li>` )}</ul>`
 
-}
+    }
